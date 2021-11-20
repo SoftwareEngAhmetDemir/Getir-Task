26c3677c6c5487452a0202193aac8fed29bed670
Ahmet Demir <43875085+SoftwareEngAhmetDemir@users.noreply.github.com>
2021-11-20 23:00:19 +0300

    Update helper-functions.js

--- Helper/helper-functions.js
+++ Helper/helper-functions.js
@@ -5,12 +5,12 @@ export  async function get_Data ({startDate, endDate,minCount,maxCount}){
  const data = await   records.aggregate([
      { 
          "$project": {
-            _id:0,
+            _id:0, // we donot need this
              key:1,
             createdAt:1,
         
           "totalCount": {
-            "$sum": "$counts" 
+            "$sum": "$counts"  //calculate the sum of counts elements
            },
         }
     }
@@ -19,8 +19,8 @@ export  async function get_Data ({startDate, endDate,minCount,maxCount}){
             
            "$match": {
                $and:[
-           { "totalCount":  {$gte:minCount, $lte: maxCount}},
-            {"createdAt":  {$gte:new Date(startDate), $lte:new Date(endDate)} } ,
+           { "totalCount":  {$gte:minCount, $lte: maxCount}}, // get the total account which is in arrange[minCount,maxCount]
+            {"createdAt":  {$gte:new Date(startDate), $lte:new Date(endDate)} } ,//get the date in arrange
           
                ]
         }  
